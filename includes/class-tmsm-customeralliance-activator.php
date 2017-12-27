fa1eca1b0aba0139e36b801a0d7f23081183e088
Nico Mollet <nico.mollet@gmail.com>
2017-12-27 14:41:01 +0100

    Merge WP Plugin Boilerplate

---
+++ includes/class-tmsm-customeralliance-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/nicomollet
+ * @since      1.0.0
+ *
+ * @package    Tmsm_Customeralliance
+ * @subpackage Tmsm_Customeralliance/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Tmsm_Customeralliance
+ * @subpackage Tmsm_Customeralliance/includes
+ * @author     Nicolas Mollet <nico.mollet@gmail.com>
+ */
+class Tmsm_Customeralliance_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-tmsm-customeralliance-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/nicomollet
+ * @since      1.0.0
+ *
+ * @package    Tmsm_Customeralliance
+ * @subpackage Tmsm_Customeralliance/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Tmsm_Customeralliance
+ * @subpackage Tmsm_Customeralliance/includes
+ * @author     Nicolas Mollet <nico.mollet@gmail.com>
+ */
+class Tmsm_Customeralliance_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
